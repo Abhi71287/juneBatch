14aae6f8ef04b0f555860df71e41467fc4be2a94
shriv <shriv@LAPTOP-TDDV87RG>
2023-08-13 16:24:07 +0530

    Day 4 commit

---
+++ Java_Concepts/src/Java_basics/Day4_Git_Concept.java
@@ -0,0 +1,10 @@
+package Java_basics;
+
+public class Day4_Git_Concept {
+	
+	public static void main(String[] args) {
+		Day4_Git_Concept obj= new Day4_Git_Concept();
+		System.out.println("Adding code to git");
+	}
+
+}
